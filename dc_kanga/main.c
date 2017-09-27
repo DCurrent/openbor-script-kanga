0208811e23b48816e37bb6439eedf462217dd0ef
Damon V. Caskey <dc@caskeys.com>
2017-09-27 11:43:42 -0400

    Add Rotate Index method.

--- dc_kanga/main.c
+++ dc_kanga/main.c
@@ -3,5 +3,6 @@
 
 // Methods.
 #import "data/scripts/dc_kanga/random_index.c"
+#import "data/scripts/dc_kanga/rotate_index.c"
 
 
---
+++ dc_kanga/rotate_index.c
@@ -0,0 +1,39 @@
+// Configuration.
+#include "data/scripts/dc_kanga/config.h"
+
+// Caskey, Damon V.
+// 2016-10-27
+//
+// Apply target entity's next map index in a continuous loop from
+// first to last.
+int dc_kanga_rotate_index()
+{
+    int index_current;  // Current palette in use.
+    int index_max;      // Number of palettes entity has loaded.
+    void target;        // Target entity.
+
+    // Get target entity.
+    target = getlocalvar(DC_KANGA_KEY_TARGET);
+
+    // If the target entity is not valid, then exit.
+    if(typeof(target) != openborconstant("VT_PTR")) return index_current;
+    if(!getentityproperty(target, "exists")) return index_current;
+
+    // Get number of maps loaded and the entity's current map index.
+    index_count     = getentityproperty(ent, "mapcount");
+    index_current   = getentityproperty(ent, "maps", "current");
+
+    // Increment to next index.
+    index_current++;
+
+    // If we've passed up the last index, reset to 0.
+    if(index_current > index_max)
+    {
+        index_current = 0;
+    }
+
+    // Apply the map index.
+    changeentityproperty(ent, "map", index_current);
+
+    return index_current;
+}
