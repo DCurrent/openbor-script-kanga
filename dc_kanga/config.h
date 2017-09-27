c0e327cdb01882b2883cb6901d0377ef70702d6b
Damon V. Caskey <dc@caskeys.com>
2017-09-27 10:55:12 -0400

    Config.
    Initial config file construction.

---
+++ dc_kanga/config.h
@@ -0,0 +1,11 @@
+#ifndef DC_KANGA
+#define DC_KANGA 1
+
+// Dependencies
+//-- Random number generator
+#include data/scripts/dc_D20/main.c
+
+// Keys.
+#define DC_KANGA_KEY_TARGET "dc_kanga_0"
+
+#endif
