e680621088cb69801e9774d2efd6bab7b5a36489
Damon V. Caskey <dc@caskeys.com>
2017-09-27 11:53:13 -0400

    Update main.c to reference new methods.

--- dc_kanga/main.c
+++ dc_kanga/main.c
@@ -2,6 +2,7 @@
 #include "data/scripts/dc_kanga/config.h"
 
 // Methods.
+#import "data/scripts/dc_kanga/increment_global_index.c"
 #import "data/scripts/dc_kanga/random_index.c"
 #import "data/scripts/dc_kanga/rotate_index.c"
 
