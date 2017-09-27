2c59a1f9cce2829828f979440a90d4cd3ef87965
Damon V. Caskey <dc@caskeys.com>
2017-09-27 11:07:00 -0400

    Add main.c

---
+++ dc_kanga/main.c
@@ -0,0 +1,11 @@
+// Configuration.
+#include data/scripts/dc_kanga/config.h
+
+// Dependencies
+//-- Random number generator
+#include data/scripts/dc_d20/main.c
+
+// Methods.
+#import data/scripts/dc_kanga/random_index.c
+
+
