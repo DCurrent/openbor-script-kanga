dcac124c22cd54013c316121d84a87d7d626b22c
Damon V. Caskey <dc@caskeys.com>
2017-09-27 11:33:02 -0400

    Move dependencies back to config.h script.

--- dc_kanga/config.h
+++ dc_kanga/config.h
@@ -1,7 +1,12 @@
 #ifndef DC_KANGA
 #define DC_KANGA 1
 
+// Dependencies
+//-- Random number generator
+#include "data/scripts/dc_d20/main.c"
+
 // Keys.
 #define DC_KANGA_KEY_TARGET "dc_kanga_0"
 
 #endif
+
--- dc_kanga/main.c
+++ dc_kanga/main.c
@@ -1,10 +1,6 @@
 // Configuration.
 #include "data/scripts/dc_kanga/config.h"
 
-// Dependencies
-//-- Random number generator
-#include "data/scripts/dc_d20/main.c"
-
 // Methods.
 #import "data/scripts/dc_kanga/random_index.c"
 
