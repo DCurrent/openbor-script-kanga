f07a96fbbf8bf3ed8de84aed2985563e546a2c1e
Caskey, Damon V <dc@caskeys.com>
2019-03-26 09:06:08 -0400

    Test activated instance. Fix base key ID constants.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #import "data/scripts/dc_kanga/random_index.c"
 #import "data/scripts/dc_kanga/rotate_index.c"
 
+#import "data/scripts/dc_kanga/instance.c"
 #import "data/scripts/dc_kanga/scale_ratio.c"
 #import "data/scripts/dc_kanga/z_position_autoscale.c"
 #import "data/scripts/dc_kanga/tint.c"
